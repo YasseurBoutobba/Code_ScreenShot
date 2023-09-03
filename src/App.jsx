8d98afb366fa236c4301caee2c0ca86d348eb0e9
yasseur_btb <y_boutobba@estin.dz>
2023-09-03 02:44:48 +0100

    Setup and Installation 🌠

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import CodeEditor from "./components/CodeEditor"
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+  return (
+    <>
+    <main className="dark min-h-screen flex justify-center items-center bg-neutral-950 text-white">
+      <CodeEditor />
+    </main>
+    </>
+  )
+}
+
+export default App
